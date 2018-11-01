aa55acee58970bc7a434f1033df77940a826fb68
Daniel Hamilton <danhamilton16@gmail.com>
2018-11-01 22:51:49 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -27,12 +27,10 @@ def bulk_tweet_shortener(array)
   end
 end
 
-def selective_tweet_shortener(array)
-  array.each do |x|
-    if x.length>140
-      puts word_substituter(x)
-    else
-      puts x
-    end
+def selective_tweet_shortener(tweet)
+  if tweet.length>140
+    word_substituter(tweet)
+  else
+    tweet
   end
 end
