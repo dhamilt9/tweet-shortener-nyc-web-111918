f1b2f40641f1e0d2b5f317f548b5da30932843e5
Daniel Hamilton <danhamilton16@gmail.com>
2018-11-01 22:46:44 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,5 @@
 def dictionary
-  output={["hello"]=>"hi", ["to", "two", "too"]=>"2", ["for", "four"]=>"4", ["be"]=>"b", ["at"]=>"@", ["and"]=>"&"}
+  output={["hello"]=>"hi", ["to", "two", "too"]=>"2", ["for", "four"]=>"4", ["be"]=>"b", ["at"]=>"@", ["and"]=>"&", ["you"]=>"u"}
   return output
 end
 
