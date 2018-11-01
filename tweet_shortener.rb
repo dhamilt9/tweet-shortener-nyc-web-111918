7735c0e1125cce90a3b13501b3ab685e3de33837
Daniel Hamilton <danhamilton16@gmail.com>
2018-11-01 22:34:11 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,3 +2,14 @@ def dictionary
   output={["hello"]=>"hi", ["to", "two", "too"]=>"2", ["for", "four"]=>"4", ["be"]=>"b", ["at"]=>"@", ["and"]=>"&"}
   return output
 end
+
+def word_substituter(tweet, dict=dictionary)
+  
+  tweet.split(" ").each do |x|
+    x.collect do |y|
+      puts y
+    end
+  end
+end
+
+word_substituter("this is a test")
